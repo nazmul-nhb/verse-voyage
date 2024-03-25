838e934894507ca54c826e4b853a20e5fa7ae04e
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-03-26 05:57:29 +0600

    made navbar and banner responsive

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,12 +2,12 @@ import { Link } from 'react-router-dom';
 import banner from '../../assets/grapes-of-wrath-banner.png';
 const Banner = () => {
     return (
-        <div className="playfair-display my-12 flex justify-between items-center px-28 py-20 bg-[#1313130D] rounded-3xl">
-            <div className="flex flex-col gap-12">
-                <h2 className="text-[#131313] text-6xl font-bold leading-normal">Books to freshen up <br /> your bookshelf</h2>
-                <Link className='work-sans bg-[#23BE0A] text-xl font-semibold text-white border border-[#23BE0A] rounded-xl md:w-48 md:h-16 hover:bg-white hover:text-[#23BE0A] transition-all flex justify-center items-center' to={'/listed-books'}>View The List</Link>
+        <div className="playfair-display my-12 flex justify-between items-center px-6 md:px-16 xl:px-28 py-6 md:py-12 xl:py-20 bg-[#1313130D] rounded-3xl mx-4 md:mx-6 xl:mx-0">
+            <div className="flex flex-col gap-4 md:gap-8 xl:gap-12">
+                <h2 className="text-[#131313] text-lg md:text-3xl xl:text-6xl font-bold md:flex md:flex-col gap-8"> <span>Books to freshen up</span> <span>your bookshelf</span></h2>
+                <Link className='work-sans bg-[#23BE0A] text-base md:text-xl font-semibold text-white border border-[#23BE0A] rounded-xl w-32 md:w-48 h-10 md:h-16 hover:bg-transparent hover:text-[#23BE0A] transition-all flex justify-center items-center' to={'/listed-books'}>View The List</Link>
             </div>
-            <div className="w-80">
+            <div className="w-40 md:w-80">
                 <img src={banner} alt="Banner" />
             </div>
         </div>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     </>
 
     return (
-        <header className="work-sans mx-0 md:mx-16 lg:mx-32 mt-3 md:mt-12">
+        <header className="work-sans mt-3 md:mt-12 mx-4 md:mx-6 xl:mx-0">
             <div className="max-w-7xl font-lexend items-center navbar mx-auto">
                 <div className="navbar-start">
                     <div className="dropdown">
@@ -23,16 +23,16 @@ const Navbar = () => {
                             {navRoutes}
                         </ul>
                     </div>
-                    <Link to={'/'} className="btn btn-ghost text-[#131313] text-lg md:text-3xl font-bold transition-all hover:shadow-sm">Verse Voyage</Link>
+                    <Link to={'/'} className="btn btn-ghost text-[#131313] text-lg md:text-3xl font-bold transition-all hover:shadow-sm px-0">Verse Voyage</Link>
                 </div>
                 <div className="navbar-center hidden xl:flex text-[#150B2BB3]">
                     <ul className="text-[#131313CC] text-lg font-semibold menu menu-horizontal gap-4 px-1">
                         {navRoutes}
                     </ul>
                 </div>
-                <div className="navbar-end flex justify-end items-center gap-4 text-lg text-white font-semibold">
-                    <button className="bg-[#23BE0A] border border-[#23BE0A] rounded-xl md:w-28 md:h-12 hover:bg-white hover:text-[#23BE0A] transition-all">Sign In</button>
-                    <button className="bg-[#59C6D2] border border-[#59C6D2] rounded-xl md:w-28 md:h-12 hover:bg-white hover:text-[#59C6D2] transition-all">Sign Up</button>
+                <div className="navbar-end flex justify-end items-center gap-2 md:gap-4 text-sm md:text-lg text-white font-semibold">
+                    <button className="bg-[#23BE0A] border border-[#23BE0A] rounded-xl w-16 md:w-28 h-7 md:h-12 hover:bg-white hover:text-[#23BE0A] transition-all">Sign In</button>
+                    <button className="bg-[#59C6D2] border border-[#59C6D2] rounded-xl w-16 md:w-28 h-7 md:h-12 hover:bg-white hover:text-[#59C6D2] transition-all">Sign Up</button>
                 </div>
             </div>
         </header>
