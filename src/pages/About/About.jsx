e1fb2e6b5dfe5f31671a33b8cfc2d48b7547b9b7
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-03-28 00:13:27 +0600

    created skeleton for about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,46 @@
 
 const About = () => {
     return (
-        <div>
-            
+        <div className="mx-6 my-4 md:my-12 text-justify">
+            <div className="">
+                <h3 className="text-center">About Us</h3>
+                <p className="indent-6">
+                    Welcome to Verse Voyage, where our passion for literature meets our love for sharing great stories. Founded by avid readers, we aim to create a vibrant community where book lovers of all ages and interests can discover their next favorite read.
+                </p>
+            </div>
+
+            <fieldset className="border border-solid border-gray-300 p-3">
+                <legend className="text-sm">Our Mission</legend>
+                <p className="">
+                    At Verse Voyage, our mission is simple: to celebrate the power of storytelling and foster a love for reading. We believe that books have the remarkable ability to inspire, educate, and entertain, and we're dedicated to helping readers find the perfect book for every occasion.
+                </p>
+            </fieldset>
+
+            <fieldset className="border border-solid border-gray-300 p-3">
+                <legend className="text-sm">What We Offer</legend>
+                <p className="">
+                    Expert Reviews: Our team of experienced reviewers provides honest, insightful critiques of a wide range of books, from bestsellers to hidden gems.
+                    Author Interviews: Get to know the faces behind the words with exclusive interviews and behind-the-scenes peeks into the lives of your favorite authors.
+                    Book Recommendations: Whether you&rsquo;re searching for your next page-turner or looking to explore a new genre, our curated lists and recommendations have you covered.
+                    Community Engagement: Join the conversation! Connect with fellow book enthusiasts, share your thoughts on recent reads, and participate in lively discussions on our blog and social media channels.
+                </p>
+            </fieldset>
+
+            <fieldset className="border border-solid border-gray-300 p-3">
+                <legend className="text-sm">Our Commitment</legend>
+                <p className="">At Verse Voyage, we&rsquo;re committed to promoting diversity and representation in literature. We strive to highlight books from diverse authors and perspectives, ensuring that everyone can find stories that resonate with them.</p>
+            </fieldset>
+
+            <fieldset className="border border-solid border-gray-300 p-3">
+                <legend className="text-sm">Get Involved</legend>
+                <p className="">
+                    Are you passionate about books and storytelling? Join our community! Whether you&rsquo;re a reader, author, or industry professional, there are plenty of ways to get involved. Follow us on social media, subscribe to our newsletter, or reach out to learn more about opportunities for collaboration.
+                </p>
+            </fieldset>
+
+            <p className="">
+                Thank you for being a part of our voyage. Together, let&rsquo;s explore the boundless world of books and celebrate the magic of reading!
+            </p>
         </div>
     );
 };
