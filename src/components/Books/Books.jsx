0c093b0ffc54fe98a6533845a4b4d38e355ba1ab
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-03-28 12:43:01 +0600

    added a button to show all books, initially it will show 6 books

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -5,6 +5,8 @@ const Books = () => {
 
     const [books, setBooks] = useState([]);
 
+    const [bookLength, setBookLength] = useState(6);
+
     useEffect(() => {
         fetch('https://raw.githubusercontent.com/nazmul-nhb/Fake-API/main/book-review/books.json')
             .then(res => res.json())
@@ -17,8 +19,13 @@ const Books = () => {
             <h2 className="playfair-display text-4xl font-bold text-center mb-9">Books</h2>
             <div className="grid min-[600px]:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
                 {
-                    books?.map(book => <Book key={book.bookID} book={book}></Book>)
+                    books?.slice(0, bookLength).map(book => <Book key={book.bookID} book={book}></Book>)
                 }
+            </div>
+            <div className={bookLength === books.length ? 'hidden' : 'flex justify-center items-center mt-8 md:mt-20'}>
+                <button
+                    onClick={() => setBookLength(books.length)}
+                    className="btn btn-primary">Show All Books</button>
             </div>
         </div>
     );
